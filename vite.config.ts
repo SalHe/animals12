a496bc14663c43f4e16799f8a34c7a4780a9c2bd
SalHe Li <SalHe@qq.com>
2023-03-12 12:36:29 +0800

    build: url root

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "animals12",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
